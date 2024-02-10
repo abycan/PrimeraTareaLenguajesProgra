39ff9c7f781b919db7f36920f4f279c56b9ec8df
abycan <159658876+abycan@users.noreply.github.com>
2024-02-10 15:45:58 -0600

    Add files via upload

---
+++ ejercicio 4.dart
@@ -0,0 +1,20 @@
+//ejercicio 4
+//Crea un programa que devuelva una lista con todos los elementos únicos 
+de otra lista.
+
+/*
+void main() {
+
+  final List<int> numeros = [1,3,4,5];
+
+  final copiaNumeros = numeros.map((numero) {
+    return numero;
+  }).toList();
+
+  print('origial: $numeros');
+  print('copia: $copiaNumeros');
+}
+ 
+  
+
+*/
