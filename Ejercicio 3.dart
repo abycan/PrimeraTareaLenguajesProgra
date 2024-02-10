64e72ef89fb5e90dd29bdc643e19d19267d7aa44
abycan <159658876+abycan@users.noreply.github.com>
2024-02-10 15:45:31 -0600

    Add files via upload

---
+++ Ejercicio 3.dart
@@ -0,0 +1,23 @@
+//Ejercicio 3
+//Crea un programa el cual imprima una lista con los primeros 15 números de 
+la sucesión de Fibonacci.
+
+/*
+ void main() {
+  const int n = 15;
+  final List<int> fibonumeros = calcularnumeros(n);
+  print("Los primeros 15 números de la sucesión de Fibonacci son:");
+  print(fibonumeros);
+}
+
+  List<int> calcularnumeros(int n) {
+   List<int> fibonumeros = [0, 1]; 
+    for (int i = 2; i < n; i++) {
+    int numero2 = fibonumeros[i - 1] + fibonumeros[i - 2];
+    fibonumeros.add(numero2);
+  }
+  return fibonumeros;
+}
+
+
+*/
