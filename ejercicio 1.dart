d4373180138ce536e2771a95a02b5dce66efa8cd
abycan <159658876+abycan@users.noreply.github.com>
2024-02-10 15:44:26 -0600

    Add files via upload

---
+++ ejercicio 1.dart
@@ -0,0 +1,27 @@
+// ejercicio 1
+/*
+
+//Crea un programa que verifique si un número es par o impar.
+
+void main() {
+  final int numero;
+ 
+
+  print(paroImpar(numero=23));
+}
+ 
+final int paroImpar(int numero){
+  if(numero%2==0){
+    print('$numero es un número par.');
+  
+    
+  }
+  else {
+    print('$numero es un número impar.');
+   
+  }
+return numero;  
+}
+
+
+*/
