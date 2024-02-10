4541023ceae693aa95977dce2df32426f441f060
abycan <159658876+abycan@users.noreply.github.com>
2024-02-10 15:45:09 -0600

    Add files via upload

---
+++ ejercicio 2.dart
@@ -0,0 +1,23 @@
+// ejercicio 2
+// . Crea un programa que sume todos los números de una lista
+
+/*
+
+void main() {
+ final List<int> numeros = [1, 2, 8, 4, 5]; 
+
+  int suma = sumarNumero(numeros);
+
+  print('La suma de los números en la lista es: $suma');
+}
+
+int sumarNumero(List<int> numeros) {
+  int suma = 0;
+  for (int i=0; i<numeros.length; i++) {
+    
+    suma += numeros[i];
+  }
+  return suma;
+}
+
+*/
